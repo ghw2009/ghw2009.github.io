d4769d186bed63f8594f54e48c339eeedf6bd675
ghw2009 <63458477+ghw2009@users.noreply.github.com>
2025-06-06 18:27:03 +0800

    Add files via upload

--- WWWROOT/Js/check.js
+++ WWWROOT/Js/check.js
@@ -1,5 +1,5 @@
 // 检查登录状态
     if (localStorage.getItem("isLoggedIn") !== "true") {
         alert("请先登录！");
-        window.location.href = "index.html";
+        window.location.href = "../../index.html";
     }
